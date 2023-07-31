65505fb1e6fb00a2902dc9e190ba11aac33851c8
Aktar Hossain <83822984+aktar11087@users.noreply.github.com>
2023-07-31 21:45:49 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,1 +1,41 @@
+#!/bin/bash
 
+# Set variables
+SERVICE_ACCOUNT_KEY='path/to/your/service_account_key.json'
+DOMAIN='domain_name'
+
+# Function to create a user
+function create_user() {
+  local first_name=$1
+  local last_name=$2
+  local email=$3
+  local employee_title=$4
+
+  # Create a JSON payload for the user
+  local payload=$(cat << EOF
+{
+  "name": {
+    "givenName": "$first_name",
+    "familyName": "$last_name"
+  },
+  "primaryEmail": "$email",
+  "password": "password123",  # Set a default password or generate one
+  "changePasswordAtNextLogin": true
+}
+EOF
+)
+
+  # Make the API call to create the user
+  curl --request POST \
+    --header "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+    --header "Content-Type: application/json" \
+    --data "$payload" \
+    "https://www.googleapis.com/admin/directory/v1/users"
+}
+
+# Read the CSV file and create users
+while IFS=, read -r first_name last_name email employee_title; do
+  create_user "$first_name" "$last_name" "$email" "$employee_title"
+done < 'path/to/your/csv_file.csv'
+
+echo "Users created successfully!"
